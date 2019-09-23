3a947dd1d50e2c9eefb1ae602273f6317115b9ed
Alessandro Casazza <imasterale@gmail.com>
2019-09-23 10:04:38 +0200

    Added typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
+const path = require('path')
+
 module.exports = {
   mode: 'development',
-  entry: ['babel-polyfill', './index.js'],
+  entry: ['./index.js'],
   output: {
     filename: 'main.js',
     publicPath: 'dist'
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env']
+      //     }
+      //   }
+      // },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        test: /\.(tsx|ts)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   }
-};
+}
