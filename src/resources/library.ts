ab5c09e6568ff8fdd35a6a53f261aabfc6e07f3f
Alessandro Casazza <alessandro@commercelayer.io>
2019-11-22 15:47:02 +0100

    feat: extended where function

--- src/resources/library.ts
+++ src/resources/library.ts
@@ -4,7 +4,19 @@ import Library from '../@types/Library'
 const subdomain = 'static-commerce'
 
 const library: Library = createResourceLibrary(
-	`https://${subdomain}.commercelayer.io/api/`
+  `https://${subdomain}.commercelayer.io/api/`
 )
+
+class ExtendLibrary extends library.Base {
+  static where(options: object): any {
+    return this.__newRelation(
+      this.__extendObjectParam('filter', {
+        q: { ...options }
+      })
+    )
+  }
+}
+
+library.Base = ExtendLibrary
 
 export default library
