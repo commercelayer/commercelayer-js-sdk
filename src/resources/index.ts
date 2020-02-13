a2b847c46e3a9197c67fa8744eeb1a95e0dd93ad
Alessandro Casazza <alessandro@commercelayer.io>
2020-02-13 12:17:57 +0100

    Alessandro - types(core): export CustomerCollection

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -78,6 +78,7 @@ export {
   CustomerPasswordResetCollection,
   CustomerPaymentSourceCollection,
   CustomerSubscriptionCollection,
+  CustomerCollection,
   DeliveryLeadTimeCollection,
   GeocoderCollection,
   GiftCardCollection,
