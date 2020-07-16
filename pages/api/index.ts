fc4aa50bbc52303c34a0d1646518234cb28e4e23
Alessandro Casazza <alessandro@commercelayer.io>
2020-07-16 19:37:01 +0200

    Ale - specs: Added for new feature getHeaders

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -12,16 +12,10 @@ export default async (__: NextApiRequest, res: NextApiResponse) => {
   }
   initCLayer(config)
   const order = await Order.withCredentials(config).find('JqXQehvOkN')
-  console.log('order._refresh', order._refresh)
-  // const sku = await Sku.find('wBeDdSgYQW')
-  // console.log('Sku', Sku)
-  // debugger
-  const attributes = {
-    _refresh: true,
-  }
-  await order.update(attributes)
-  console.log('updateOrder', order._refresh)
+  const sku = await Order.withCredentials(config).all()
   res.status(200).json({
     success: true,
+    orderHeaders: order.getHeaders(),
+    skuHeaders: sku.getHeaders(),
   })
 }
